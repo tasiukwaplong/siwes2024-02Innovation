8a445aa221826f201f255ebfd82714264b52a456
TK <tasiukwaplong@gmail.com>
2024-07-31 11:02:43 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+//echo "Hello world";
+//echo phpversion();
+
+/* ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>"; */
+
+$age = 12;
+$cgpa = 2.1;
+$name = "AIsha Abdullahi";
+
+$aGe = 42;
+$Cpga = 3.5;
+$Name = "Faruq Tunga";
+
+/*echo "$Name is $aGe years old";
+echo "<br>";
+echo $name." is ".$age." years old";
+echo "<br>";
+echo $age + $aGe;
+echo "<br>";
+echo $age . $aGe;
+echo "<br>";
+var_dump($age);
+echo "<br>";
+var_dump($name);
+$cgpa = array(4.2, 2.91, 3.8, 1.8, 3.42, 0.99);
+echo "<br>";
+echo $cgpa[2];
+echo "<br>";
+echo $cgpa[0];
+echo "<br>";
+print_r($cgpa);
+echo "<br>";
+$age = (string) $age;
+echo $age + $aGe;
+echo "<br>";
+var_dump($age);*/
+
+// echo strlen('The man');
+// echo str_word_count('The man');
+// echo strpos("FULAFIA is the best institution in Nasarawa State", "the");
+
+// echo PHP_INT_MAX;
+// echo "<br>";
+// echo PHP_INT_MIN;
+// echo "<br>";
+// echo PHP_INT_SIZE;
+// $radius = 2;
+
+// echo pi()*$radius*$radius;
+// echo min(4.2, 2.91, 3.8, 1.8, 3.42, 0.99);
+// echo abs(-2);
+// echo rand();
+
+?>
