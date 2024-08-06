80c72cd2e8a579cb9e5e407bbe2fa064eaa03c82
TK <tasiukwaplong@gmail.com>
2024-08-06 16:59:49 +0100

    FX: view todos

---
+++ todo/backend.php
@@ -0,0 +1,13 @@
+<?php
+// echo $_POST['item'];
+
+$todos = array(
+    "Open the office by 9am",
+    "Sweep the office",
+    "Arrange the chairs",
+    "Wash the toilet",
+    "Attend class",
+    "Do assignment"
+);
+
+?>
--- todo/index.php
+++ todo/index.php
@@ -1,3 +1,16 @@
+<?php
+$todos = array(
+    "Open the office by 9am",
+    "Sweep the office",
+    "Arrange the chairs",
+    "Wash the toilet",
+    "Attend class",
+    "Do assignment",
+    "Watch Anime"
+);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +22,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <div class="container mt-5">
+    <div class="container mt-5 mb-5">
         <div class="row">
             <div class="col-md-12">
                 <h1 class="text-center">TO-DO App Using PHP</h1>
@@ -17,8 +30,8 @@
 
             <!-- colunmn for form to add todo -->
             <div class="col-md-6">
-                <form action="">
-                    <textarea name="" id="" cols="20" rows="10" class="form-control"></textarea>
+                <form action="backend.php" method="POST">
+                    <textarea name="item" id="" cols="20" rows="10" class="form-control"></textarea>
                     <button type="submit" class="btn btn-primary btn-block">Add <i class="fa fa-plus"></i></button>
                     <button type="cancel" class="btn btn-danger">Clear <i class="fa fa-trash"></i> </button>
                 </form>
@@ -28,9 +41,11 @@
             <div class="col-md-6">
                 <div class="row">
 
-
+                    <?php
+                        for ($x = 0; $x <= (count($todos) - 1); $x++):
+                    ?>
                     <div class="col-md-10 p-3 border border-4">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, optio voluptates repellendus quidem, aspernatur maiores ducimus officiis veritatis cum vel dolor quaerat. Aliquid nostrum corporis molestias laboriosam, ad officia itaque.
+                       <?php echo $todos[$x];?>
                         <p class="float-end mt-3">
                             <small>
                                 <strong>Status: </strong> Not done
@@ -39,77 +54,16 @@
                     </div>
                     <div class="col-md-2">
                         <div class="d-grid gap-2">
-                            <button class="btn btn-danger" type="button">
+                            <button class="btn btn-sm btn-danger" type="button">
                                 <i class="fa fa-trash-o"></i>
                             </button>
-                            <button class="btn btn-secondary" type="button">
+                            <button class="btn btn-sm btn-secondary" type="button">
                                 <i class="fa fa-pencil"></i>
                             </button>
                         </div>
                     </div>
 
-
-
-                    <div class="col-md-10 p-3 border border-4">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, optio voluptates repellendus quidem, aspernatur maiores ducimus officiis veritatis cum vel dolor quaerat. Aliquid nostrum corporis molestias laboriosam, ad officia itaque.
-                        <p class="float-end mt-3">
-                            <small>
-                                <strong>Status: </strong> Not done
-                            </small>
-                        </p>
-                    </div>
-                    <div class="col-md-2">
-                        <div class="d-grid gap-2">
-                            <button class="btn btn-danger" type="button">
-                                <i class="fa fa-trash-o"></i>
-                            </button>
-                            <button class="btn btn-secondary" type="button">
-                                <i class="fa fa-pencil"></i>
-                            </button>
-                        </div>
-                    </div>
-
-
-                    <div class="col-md-10 p-3 border border-4">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, optio voluptates repellendus quidem, aspernatur maiores ducimus officiis veritatis cum vel dolor quaerat. Aliquid nostrum corporis molestias laboriosam, ad officia itaque.
-                        <p class="float-end mt-3">
-                            <small>
-                                <strong>Status: </strong> Not done
-                            </small>
-                        </p>
-                    </div>
-                    <div class="col-md-2">
-                        <div class="d-grid gap-2">
-                            <button class="btn btn-danger" type="button">
-                                <i class="fa fa-trash-o"></i>
-                            </button>
-                            <button class="btn btn-secondary" type="button">
-                                <i class="fa fa-pencil"></i>
-                            </button>
-                        </div>
-                    </div>
-
-
-
-                    <div class="col-md-10 p-3 border border-4">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, optio voluptates repellendus quidem, aspernatur maiores ducimus officiis veritatis cum vel dolor quaerat. Aliquid nostrum corporis molestias laboriosam, ad officia itaque.
-                        <p class="float-end mt-3">
-                            <small>
-                                <strong>Status: </strong> Not done
-                            </small>
-                        </p>
-                    </div>
-                    <div class="col-md-2">
-                        <div class="d-grid gap-2">
-                            <button class="btn btn-danger" type="button">
-                                <i class="fa fa-trash-o"></i>
-                            </button>
-                            <button class="btn btn-secondary" type="button">
-                                <i class="fa fa-pencil"></i>
-                            </button>
-                        </div>
-                    </div>
-
+                    <?php endfor;?>
                     
                 </div>
             </div>
